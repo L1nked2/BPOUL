7b657b06975578df840971abe7bb4d186d2ba0f4
L1nked23 <leeerjin@gmail.com>
2022-11-15 12:35:53 +0900

    Update datacollection.py

--- src/datacollection.py
+++ src/datacollection.py
@@ -8,13 +8,12 @@ def get_data(region='KR'):
     start_date = '2010-12-30'
     end_date = '2021-12-30'
     df_list = []
-    num_stocks = 400
+    num_stocks = 300
     for code in df_krx["Code"][:num_stocks]:
-        series_close = fdr.DataReader('KRX', code, start_date, end_date)["Close"]
+        series_close = fdr.DataReader(code, start_date, end_date, 'KRX')["Close"]
         df_list.append(series_close)
     df_close = pd.concat(df_list, axis=1)
     df_close.columns = df_krx["Name"][:num_stocks]
-    print(df_close.columns)
     df_close = df_close.dropna(axis=1)
     df_returns = df_close.pct_change()
     df_returns = df_returns.iloc[1:, :]
